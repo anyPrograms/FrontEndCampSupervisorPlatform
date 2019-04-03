71f189d53dfbab59495027de478c52445051f571
I506623 <luke.liang@sap.com>
2019-04-03 17:38:18 +0800

    0403

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ class App extends Component<any, state>{
     super(props);
     const pathname: string = location.pathname;
     this.state = {
-      current: pathname.slice(1) || 'home',
+      current: pathname.split('/')[1] || 'home',
     }
   }
 
@@ -21,68 +21,76 @@ class App extends Component<any, state>{
     });
   }
 
-  renderMenu(props?: any) {
-    // const menu = <Menu
-    //   onClick={this.handleClick}
-    //   selectedKeys={[this.state.current]}
-    //   mode="horizontal"
-    //   className="display-flex horizontal-center border-none tab-container-menu"
-    // >
-    //   <Menu.Item key="home">
-    //     <Link to="/">HOME</Link>
-    //   </Menu.Item>
-    //   <Menu.Item key="shop">
-    //     <Link to="/shop">SHOP</Link>
-    //   </Menu.Item>
-    //   <Menu.Item key="blog">
-    //     <a href="https://ant.design" rel="noopener noreferrer">BLOG</a>
-    //   </Menu.Item>
-    //   <Menu.Item key="faq">
-    //     <a href="https://ant.design" rel="noopener noreferrer">FAQ</a>
-    //   </Menu.Item>
-    //   <Menu.Item key="contact">
-    //     <a href="https://ant.design" rel="noopener noreferrer">CONTACT</a>
-    //   </Menu.Item>
-    // </Menu>;
-    const onClick = (evt: any) => {
-      const node = evt.currentTarget;
-      const parentElement = node.parentElement;
-      const selectedNode = parentElement.querySelector('[data-selected=true]')
-      selectedNode && selectedNode.removeAttribute('data-selected');
-      node.setAttribute('data-selected', true);
-    }
-    const data = [<Link to="/">HOME</Link>, <Link to="/shop">SHOP</Link>, <Link to="/blog">BLOG</Link>, <Link to="/faq">FAQ</Link>, <Link to="/contact">CONTACT</Link>]
-    const menu = <ul className="tab-container-menu">
-      {data.map((node, key) => {
-        return (
-          <li key={key} onClick={onClick}>{node}</li>
-        )
-      })}
-    </ul>
-
-    return menu;
-  }
-
   render() {
     return (
       <BrowserRouter>
-
         <div className="main-content">
           <div className="display-flex tab-container horizontal-space-between">
             <a href="/" className="display-flex align-items-center tab-logon">
               <img src={require('./img/logon.png')} className="display-block" />
             </a>
-            {this.renderMenu()}
+            <TabMenu selectedKey={this.state.current}>
+              <TabMenu.Item key="home"><Link to="/">HOME</Link></TabMenu.Item>
+              <TabMenu.Item key="shop"><Link to="/shop">SHOP</Link></TabMenu.Item>
+              <TabMenu.Item key="blog"><Link to="/blog">BLOG</Link></TabMenu.Item>
+              <TabMenu.Item key="faq"><Link to="/faq">FAQ</Link></TabMenu.Item>
+              <TabMenu.Item key="contact"><Link to="/contact">CONTACT</Link></TabMenu.Item>
+            </TabMenu>
           </div>
           <div className="sub-content">
             <AppRoute></AppRoute>
           </div>
-
         </div>
       </BrowserRouter>
 
     );
   }
+}
+
+class TabMenu extends Component<any> {
+  state = { selectedKey: this.props.selectedKey, visible: false };
+
+  static Item = (props: any) => {
+    const onclick = () => {
+      props.parentsClickHandle && props.parentsClickHandle(props.eventKey);
+    }
+    return (
+      <li onClick={onclick} data-selected={props.selected}>{props.children}</li>
+    )
+  };
+
+  onClick = (selectedKey: string) => {
+    this.setState({
+      selectedKey
+    });
+  }
+
+  displayMenuList = () => {
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+
+  render() {
+    const menu = <div className="tab-container-menu">
+      <Button icon="menu" className="tab-container-menu-button" onClick={this.displayMenuList} />
+      <ul className={`tab-container-menu-list ${this.state.visible ? 'tab-container-menu-list-visible' : ''}`}>
+        {React.Children.map(this.props.children, (child: any) => {
+          const props: any = {
+            parentsClickHandle: this.onClick,
+            eventKey: child.key
+          }
+          if (child.key === this.state.selectedKey) {
+            props['selected'] = true;
+          }
+          return React.cloneElement(child, props);
+        })}
+      </ul>
+    </div>
+
+    return menu;
+  }
+
 }
 
 export default App;
