3e2a8ea042e6b20a7c6f5775426a89fcef5c591a
JudyZhu <zhubingyu.ice@gmail.com>
2019-06-02 19:27:41 +0800

    0602

--- src/component/RoomManage.tsx
+++ src/component/RoomManage.tsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect, useCallback, ReactNode } from 'react';
+import React, { useState, useEffect, ReactNode, useMemo } from 'react';
 import { Button, Statistic, Modal, Transfer, Pagination, Card, } from 'antd';
 import { baseUrl } from '../url/url';
 
-const formatCampers = (campers: any, campersOfChosenRoom: any) => {
-    const campersOfNotChosenRoom = campers.filter(({ studentId }: any) => {
-        const isSelected = campersOfChosenRoom.find(({ key }: any) => key === studentId);
-        return !isSelected;
+const formatCampers = (undistributedCampers: any, distributedCampers: any) => {
+    const allFormatCampers = distributedCampers.concat(undistributedCampers).map((camper: any) => {
+        return {
+            key: camper['studentId'],
+            title: camper['studentName'],
+            chosen: false
+        }
     });
 
-    const campersWithFormatOfNotChosenRoom = campersOfNotChosenRoom.map((camper: any) => (
-        {
-            key: camper['studentId'],
-            title: camper['studentName'],
-            chosen: false
-        })
-    );
-
-    const campersWithFormatOfChosenRoom = campersOfChosenRoom.map((camper: any) => (
-        {
-            key: camper['studentId'],
-            title: camper['studentName'],
-            chosen: false
-        })
-    );
-
-    const allFormatCampers = campersWithFormatOfNotChosenRoom.concat(campersWithFormatOfChosenRoom);
-
-    const targetKeys = campersWithFormatOfChosenRoom.map(({ key }: any) => key);
+    const targetKeys = distributedCampers.map(({ studentId }: any) => { return studentId });
     return {
         dataSource: allFormatCampers,
         targetKeys
@@ -38,8 +23,8 @@ const Rooms = (props: any) => {
     const [dataSource, setDataSource] = useState([]);
     const [targetKeys, setTargetKeys] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
-    let chosenRoom: any = {};
-    const { allBunks, distributedCampers, undistributedCampers } = props;
+    const [chosenRoom, setChosenRoom] = useState({});
+    const { allBunks, distributedCampers, undistributedCampers, setStudentToBunk, getDistributedCampersByBunkId, getUndistributedCampersData } = props;
     const filterOption = (inputValue: string, option: any) => option.description.indexOf(inputValue) > -1
 
     const closeModal = () => {
@@ -47,12 +32,16 @@ const Rooms = (props: any) => {
     }
 
     const openModel = (bunk: any) => {
-        chosenRoom = bunk;
-        const allCampers = distributedCampers.concat(undistributedCampers);
-        const { dataSource, targetKeys } = formatCampers(allCampers, chosenRoom.campers);
+        setChosenRoom(bunk);
+        getDistributedCampersByBunkId(bunk['bunkId']);
+        getUndistributedCampersData();
+    }
+
+    useEffect(() => {
+        const { dataSource, targetKeys } = formatCampers(undistributedCampers, distributedCampers);
         setDataSource(dataSource);
         setTargetKeys(targetKeys);
-    }
+    }, [distributedCampers, undistributedCampers])
 
     const clickHandle = (bunk: any) => {
         setModalVisible(true);
@@ -61,14 +50,14 @@ const Rooms = (props: any) => {
 
     const renderRooms = () => {
         return (
-            <Card>{allBunks.map((bunk: any, key: number) => (
-                <Card.Grid key={key} style={{ padding: 0, width: '200px' }}>
+            <Card>{allBunks.map((bunk: any) => (
+                <Card.Grid key={bunk['bunkId']} style={{ padding: 0, width: '200px' }}>
                     <div onClick={() => { clickHandle(bunk) }} style={{
                         textAlign: 'center',
                         padding: '2em'
                     }}>
-                        <p >房间名称: {bunk.bunkName}</p>
-                        <p>当前分配: {bunk.campers.length + ` / ${bunk.bunkVol}`}</p>
+                        <p >房间名称: {bunk['bunkName']}</p>
+                        <p>当前分配: {bunk['existedNum'] + ` / ${bunk['bunkVol']}`}</p>
                     </div>
 
                 </Card.Grid>
@@ -80,15 +69,15 @@ const Rooms = (props: any) => {
         setTargetKeys(targetKeys as []);
         if (direction === 'right')//房间新增的人员
         {
-
+            setStudentToBunk(moveKeys[0], (chosenRoom as any)['bunkId']);
         } else {
-
+            setStudentToBunk(moveKeys[0], '');
         }
     }
 
     return (
         <div style={{ overflow: 'hidden', display: 'flex' }}>
-            <Modal visible={modalVisible} onCancel={closeModal} footer={null} style={{ textAlign: 'center' }} centered title={chosenRoom['bunkName'] || ''}>
+            <Modal visible={modalVisible} onCancel={closeModal} footer={null} style={{ textAlign: 'center' }} centered title={(chosenRoom as any)['bunkName'] || ''}>
                 <Transfer
                     listStyle={{ textAlign: "left" }}
                     dataSource={dataSource}
@@ -118,72 +107,60 @@ const CreateRoom = () => {
 }
 
 const RoomManage = () => {
+    const [allBunks, setAllBunks] = useState([]);
+    const [countUndistributed, setCountUndistributed] = useState(0);
+    const [countDistributed, setCountDistributed] = useState(0);
     const [undistributedCampers, setUndistributedCampers] = useState([]);
     const [distributedCampers, setDistributedCampers] = useState([]);
-    const [allBunks, setAllBunks] = useState([]);
-
-    const getAllCampersData = () => {
-        fetch(baseUrl + 'csp/con/student/all').then(data => data.json()).then(campers => {
-            const undistributedCampers: any[] = [];
-            const distributedCampers: any[] = [];
-            campers.forEach((camper: any) => {
-                if (camper['studentBunk']) {
-                    distributedCampers.push(camper);
-                } else {
-                    undistributedCampers.push(camper);
-                }
-            });
-            setUndistributedCampers(undistributedCampers as never[]);
-            setDistributedCampers(distributedCampers as never[]);
-        });
+    const getCountUndistributed = () => {
+        fetch(baseUrl + 'csp/con/student/countUndistributed')
+            .then(data => data.json())
+            .then(num => setCountUndistributed(num));
     };
 
-    const getAllUndistributedCampersData = () => {
-        fetch(baseUrl + 'con/student/undistributed').then(data => data.json()).then(campers => {
-            setUndistributedCampers(campers);
-        });
+    const getCountDistributed = () => {
+        fetch(baseUrl + 'csp/con/student/countDistributed')
+            .then(data => data.json())
+            .then(num => setCountDistributed(num));
+    };
+
+    const getUndistributedCampersData = () => {
+        fetch(baseUrl + 'csp/con/student/undistributed')
+            .then(data => data.json())
+            .then(json => setUndistributedCampers(json));
     }
 
     const getDistributedCampersByBunkId = (id: string) => {
-        fetch(baseUrl + 'con/student/findStudentsByBunk/' + id).then(data => data.json()).then(campers => {
-            setDistributedCampers(campers);
-        });
+        fetch(baseUrl + 'csp/con/student/findStudentsByBunk/' + id)
+            .then(data => data.json())
+            .then(json => setDistributedCampers(json));
+    }
+
+    const setStudentToBunk = (studentId: string, bunkId: string) => {
+        fetch(baseUrl + `csp/con/student/addStudentToBunk?studentId=${studentId}&bunkId=${bunkId}`, { method: 'post' }).then(data => data.ok).then(refreshData);
     }
 
     const getAllBunksData = () => {
         fetch(baseUrl + 'csp/con/bunk/all')
             .then(data => data.json())
             .then(rooms => {
-                const campersOfRooms = rooms.map(({ bunkId }: any) => fetch(baseUrl + 'csp/con/student/findStudentsByBunk/' + bunkId).then(data => data.json()));
-
-                Promise.all(campersOfRooms)
-                    .then(campersOfRooms => {
-                        campersOfRooms.forEach((campersOfRoom, index) => {
-                            Object.assign(rooms[index], { campers: campersOfRoom });
-                        });
-
-                        setAllBunks(rooms);
-                    });
-
+                setAllBunks(rooms);
             }).catch(e => console.log(e));
     }
-
-    const refreshAllBunksData = () => {
+    const refreshData = () => {
         getAllBunksData();
+        getCountDistributed();
+        getCountUndistributed();
     }
-
-    useEffect(() => {
-        getAllCampersData();
-        getAllBunksData();
-    }, []);
+    useEffect(refreshData, []);
 
     return (
         <div className="room-manage-content">
             <div style={{ display: 'flex', justifyContent: 'center', boxShadow: '2px 0 8px rgba(0, 0, 0, 0.15)', padding: '1em 0' }}>
-                <Statistic title="未分配的人数" value={undistributedCampers.length} valueStyle={{ textAlign: "center", color: 'green' }} />
-                <Statistic title="已分配的人数" value={distributedCampers.length} style={{ marginLeft: '2em' }} valueStyle={{ textAlign: "center", color: 'red' }} />
+                <Statistic title="未分配的人数" value={countUndistributed} valueStyle={{ textAlign: "center", color: 'green' }} />
+                <Statistic title="已分配的人数" value={countDistributed} style={{ marginLeft: '2em' }} valueStyle={{ textAlign: "center", color: 'red' }} />
             </div>
-            <Rooms {...{ allBunks, distributedCampers, undistributedCampers }}></Rooms>
+            <Rooms {...{ allBunks, undistributedCampers, distributedCampers, getDistributedCampersByBunkId, getUndistributedCampersData, setStudentToBunk }}></Rooms>
             <CreateRoom />
         </div>
     )
